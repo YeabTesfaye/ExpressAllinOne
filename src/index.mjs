5fb2e2bb4b4b1fe1a474aa326977b026002eefc8
Yeabsra <tesfayeyeabsra446@gmail.com>
2024-07-02 22:01:04 +0300

    session and cookie added

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,27 @@
 import express from "express";
-import routers from './routes/index.mjs';
-import userRouter from "./routes/users.mjs";
-import productRouter from './routes/products.mjs';
+import routers from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "@#$@$@#Hello@#@$#@$",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 24,
+    },
+  })
+);
 app.use(routers);
 
-
+app.get("/", (req, res) => {
+  console.log(req.session);
+  console.log(req.session.id);
+  req.session.visited = true;
+  res.cookie("hello", "world", { maxAge: 1000 });
+  return res.send("hello");
+});
 export default app;
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -2,7 +2,9 @@ import { Router } from "express";
 import { mockProducts } from "../utils/constants.mjs";
 const router = Router();
 
-router.get("/api/products", (req, res) => {
+router.get("", (req, res) => {
+  console.log(req.headers.cookie);
+  console.log(req.cookies);
   res.status(200).send(mockProducts);
 });
 
